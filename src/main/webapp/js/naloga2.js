96a7a7de5dc7fa0ead7b0f83300fd69a29e88196
Mitja Uršič <odtihmal@gmail.com>
2013-12-21 17:08:35 +0100

    Simplify JavaScript.

--- src/main/webapp/js/naloga2.js
+++ src/main/webapp/js/naloga2.js
@@ -1,13 +1,5 @@
-function set_select_all() {
-    $("[id*='selectAll']").click(function() {
-        if ($(this).prop('checked')) {
-            $('.checkBox').find('input:checkbox').prop('checked', true);
-        } else {
-            $('.checkBox').find('input:checkbox').prop('checked', false);
-        }
-    });
-}
-
 $(document).ready(function() {
-    set_select_all();
+    $("[id*='selectAll']").click(function() {
+        $('.checkBox').find('input:checkbox').prop('checked', $(this).prop('checked'));
+    });
 });
